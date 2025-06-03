e5d8079e52e628f1703fd6cc7d46d878fd652738
Coming98 <jcqueue@gmail.com>
2025-06-02 23:55:18 -0400

    ARMOUR Illustration 1.0

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,18 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const dev = process.argv.includes('dev');
 
 export default {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
+	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
-            pages: 'build',
-            assets: 'build',
-            fallback: null,
-            precompress: false,
-            strict: true
-        }),
-		paths: { base: '/SensorGuardIllustration' }
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false,
+			strict: true
+		}),
+		paths: { base: dev ? '' : '/SensorGuardIllustration' }
 	}
 };
